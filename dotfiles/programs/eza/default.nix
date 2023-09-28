9d1b0cdc1b257310bc93d8725cbdfe4252289e03
koalagang <74791721+koalagang@users.noreply.github.com>
2023-09-28 20:56:16 +0100

    Added sl alias to eza for when I typo

--- dotfiles/programs/eza/default.nix
+++ dotfiles/programs/eza/default.nix
@@ -6,6 +6,8 @@
 
   home.shellAliases = {
     ls = "eza --all --long --no-permissions --no-user --no-time --group-directories-first --sort=size --binary";
+    # for when I typo
+    sl = "eza --all --long --no-permissions --no-user --no-time --group-directories-first --sort=size --binary";
     ll = "eza --long";
     tree = "eza --tree";
   };
