a83753041cc9665ac80a7e883a107d64d5921edb
koalagang <74791721+koalagang@users.noreply.github.com>
2023-09-29 20:03:59 +0100

    Expan shell config

--- dotfiles/misc/shell/default.nix
+++ dotfiles/misc/shell/default.nix
@@ -1,30 +1,6 @@
-{ config, ... }:
-
 {
-  # TODO: functionrc with writeShellScriptBin
-
-  home = {
-    # Besides iwc (which will be removed later),
-    # these are all the "misc" aliases I use.
-    # Most aliases are located in the file for the respective tool.
-    # For instance, in zathura's nix config I've defined za = "zathura"
-    shellAliases = {
-      grep = "grep --colour=auto";
-
-      desk = "cd ${config.xdg.userDirs.desktop}";
-      doc = "cd ${config.xdg.userDirs.documents}";
-      dl = "cd ${config.xdg.userDirs.download}";
-      pix = "cd ${config.xdg.userDirs.pictures}";
-      vid = "cd ${config.xdg.userDirs.videos}";
-      gdir = "cd ${config.xdg.userDirs.desktop}/git";
-
-      "..." = "cd ../..";
-      "...." = "cd ../../..";
-      # TODO: write a function that allows you to do any number+dots
-      # e.g. 7.. should send you seven directories up
-
-      # TODO: write iwtui (an iwd tui) using whiptail
-      iwc = "iwctl station wlan0";
-    };
-  };
+  imports = [
+    ./aliasrc.nix
+    ./functionrc.nix
+  ];
 }
---
+++ dotfiles/misc/shell/functionrc.nix
@@ -0,0 +1,57 @@
+{ pkgs, ... }:
+
+{
+  home.packages = [
+    # Extract an archive (compressed files/folders)
+    (pkgs.writeShellScriptBin "ex" ''
+      if [ -f "$1" ] ; then
+          case "$1" in
+              # unlikely that I'll need these
+              #*.7z)        7z x "$1"       ;;
+              #*.deb)       ar x "$1"       ;;
+              #*.tar.zst)   unzstd "$1"     ;;
+              #*.rar)       unrar x "$1"    ;; # unrar is unfree
+              *.Z)         ${pkgs.gzip}/bin/uncompress "$1"     ;;
+              *.bz2)       ${pkgs.bzip2}/bin/bunzip2 "$1" ;;
+              *.gz)        ${pkgs.gzip}/bin/gunzip "$1"         ;;
+              *.tar)       ${pkgs.gnutar}/bin/tar xf "$1"       ;;
+              *.tar.bz2)   ${pkgs.gnutar}/bin/tar xjf "$1"      ;;
+              *.tar.gz)    ${pkgs.gnutar}/bin/tar xf "$1"       ;;
+              *.tar.xz)    ${pkgs.gnutar}/bin/tar xf "$1"       ;;
+              *.tbz2)      ${pkgs.gnutar}/bin/tar xjf "$1"      ;;
+              *.tgz)       ${pkgs.gnutar}/bin/tar xzf "$1"      ;;
+              *.zip)       ${pkgs.unzip}/bin/unzip "$1"         ;;
+              *)           ${pkgs.coreutils}/bin/echo "'$1' cannot be extracted with ex"
+          esac
+      else
+          ${pkgs.coreutils}/bin/echo "error: '$1' is not a valid file"
+      fi
+    '')
+
+    # Compress a folder or a file
+    (pkgs.writeShellScriptBin "comp" ''
+      case "$2" in
+          '-t')   ${pkgs.gnutar}/bin/tar -c --auto-compress -f "$1.tar.gz" "$1" ;;
+          '-z')   ${pkgs.gnutar}/bin/tar -c --auto-compress -f "$1.zip" "$1" ;;
+          *)      ${pkgs.gnutar}/bin/tar -c --auto-compress -f "$1.tar.gz" "$1"
+      esac
+    '')
+
+    # Backup a file
+    (pkgs.writeShellScriptBin "bak" ''
+      ${pkgs.coreutils}/bin/cp --backup=numbered "$1" "$1.bak"
+    '')
+
+    # Upload content to 0x0.st (a pastebin and URL shortener)
+    (pkgs.writeShellScriptBin "oxo" ''
+      while getopts 's:f:u:' OPT; do
+        case $OPT in
+          s) ${pkgs.curl}/bin/curl -F "shorten=$OPTARG" http://0x0.st ;;
+          f) ${pkgs.curl}/bin/curl -F "file=@$OPTARG" http://0x0.st   ;;
+          u) ${pkgs.curl}/bin/curl -F "url=$OPTARG" http://0x0.st     ;;
+          *) ${pkgs.coreutils}/bin/echo 'error: invalid flag' # for some reason it won't get invoked
+        esac
+      done
+    '')
+  ];
+}
