1384f31424eac260bccee8b1a22d7a696689a748
koalagang <74791721+koalagang@users.noreply.github.com>
2023-09-29 11:45:48 +0100

    Revert "Added sl alias to eza for when I typo" because it causes an infinite recursion
    This reverts commit 9d1b0cdc1b257310bc93d8725cbdfe4252289e03.

--- dotfiles/programs/eza/default.nix
+++ dotfiles/programs/eza/default.nix
@@ -6,8 +6,6 @@
 
   home.shellAliases = {
     ls = "eza --all --long --no-permissions --no-user --no-time --group-directories-first --sort=size --binary";
-    # for when I typo
-    sl = "eza --all --long --no-permissions --no-user --no-time --group-directories-first --sort=size --binary";
     ll = "eza --long";
     tree = "eza --tree";
   };
