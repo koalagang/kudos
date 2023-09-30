489369a466c2e01564dd6b90ec9d4dd9a623b214
koalagang <74791721+koalagang@users.noreply.github.com>
2023-09-30 17:58:12 +0100

    Added comment to clarify the purpose of shell being separate to zsh

--- dotfiles/misc/shell/default.nix
+++ dotfiles/misc/shell/default.nix
@@ -1,3 +1,7 @@
+# NOTE: if you're looking for my shell of choice, you're in the wrong place.
+# My zsh configuration is in programs/zsh.
+# This config here just consists of my general shell-neutral stuff,
+# i.e. aliases and functions.
 {
   imports = [
     ./aliasrc.nix
