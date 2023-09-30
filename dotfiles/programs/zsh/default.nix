6fe056971365a49ad29f30893ffb12eabaa46cb5
koalagang <74791721+koalagang@users.noreply.github.com>
2023-09-30 17:55:52 +0100

    nix-zsh-completions plugin

--- dotfiles/programs/zsh/default.nix
+++ dotfiles/programs/zsh/default.nix
@@ -80,6 +80,18 @@
       # TODO: nix-colors
       #styles = {};
     };
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.7.0";
+          sha256 = "149zh2rm59blr2q458a5irkfh82y3dwdich60s9670kl3cl5h2m1";
+        };
+      }
+    ];
   };
 
   # My favourite prompt
