c7b89e6a05c221fdfefb22b8a7db3669016270bc
koalagang <74791721+koalagang@users.noreply.github.com>
2023-10-02 12:23:05 +0100

    Tidy up aliasrc

--- dotfiles/misc/shell/aliasrc.nix
+++ dotfiles/misc/shell/aliasrc.nix
@@ -1,31 +1,31 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 
 {
   # Besides iwc (which will be removed later),
   # these are all the "misc" aliases I use.
-  # Most aliases are located in the file for the respective tool.
+  # Most aliases are located in the file for the respective software.
   # For instance, in zathura's nix config I've defined za = "zathura"
   home.shellAliases = {
     grep = "grep --colour=auto";
-    G="grep --colour=auto";
-    A="awk";
-    C="cut";
-    E="echo";
-    S="sed";
+    G = "grep --colour=auto";
+    A = "awk";
+    C = "cut";
+    E = "echo";
+    S = "sed";
 
-    desk = "cd ${config.xdg.userDirs.desktop}";
-    doc = "cd ${config.xdg.userDirs.documents}";
-    dl = "cd ${config.xdg.userDirs.download}";
-    pix = "cd ${config.xdg.userDirs.pictures}";
-    vid = "cd ${config.xdg.userDirs.videos}";
     gdir = "cd ${config.xdg.userDirs.desktop}/git";
+    desk = "cd ${config.xdg.userDirs.desktop}";
+    doc  = "cd ${config.xdg.userDirs.documents}";
+    dl   = "cd ${config.xdg.userDirs.download}";
+    pic  = "cd ${config.xdg.userDirs.pictures}";
+    vid  = "cd ${config.xdg.userDirs.videos}";
 
     "..." = "cd ../..";
     "...." = "cd ../../..";
-    # TODO: write a function that allows you to do any number+dots
-    # e.g. 7.. should send you seven directories up
+    # TODO: write a function that allows you to write any number+dots
+    # e.g. 7. should send you seven directories up
 
-    # TODO: write iwtui (an iwd tui) using whiptail
+    # TODO: write iwtui (an iwd tui) using dialog
     iwc = "iwctl station wlan0";
   };
 }
