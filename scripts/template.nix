f0b171cf66003e24aa1a0b1b44335128ac77f543
koalagang <74791721+koalagang@users.noreply.github.com>
2024-01-04 18:45:47 +0000

    Add nix template for scripts

---
+++ scripts/template.nix
@@ -0,0 +1,14 @@
+{ pkgs, config, ... }:
+
+# A simple template for adding bash scripts to your nix config
+# You can remove the above 'config' if you're not using any homemanager variables
+
+{
+  home = {
+    packages = [
+      (pkgs.writeShellScriptBin "foobar" ''
+        echo 'hello world'
+      '')
+    ];
+  };
+}
