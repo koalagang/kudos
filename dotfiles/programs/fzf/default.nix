e229956ada1fbb4fc0b2bb0cb1a8c5625ed30283
koalagang <74791721+koalagang@users.noreply.github.com>
2024-01-04 18:21:57 +0000

    Add some comments to fzf config

--- dotfiles/programs/fzf/default.nix
+++ dotfiles/programs/fzf/default.nix
@@ -5,22 +5,26 @@
     enable = true;
 
     # Enable fzf widgets for any shell installed and configured by home-manager
-    enableBashIntegration    = true;
-    enableFishIntegration    = true;
-    enableZshIntegration     = true;
+    enableBashIntegration = true;
+    enableFishIntegration = true;
+    enableZshIntegration  = true;
     # NOTE: fzf does not have nushell integration
 
     # TODO: stylix
     #colors = {};
+
+    defaultCommand = "${pkgs.bfs}/bin/bfs -nohidden -type f";
+    #defaultOptions = {};
 
     /* -- fzf shell widgets
     alt + c = search with fzf and cd into output
     ctrl + r = search history and paste output onto the commandline
     ctrl + t = search for files and paste output onto the commandline
     */
+
+    # -- alt + c widget
     # use bfs instead of find or fd (much faster; so fast I never even see fzf's loading symbol)
     changeDirWidgetCommand = "${pkgs.bfs}/bin/bfs -nohidden -type d";
-
     changeDirWidgetOptions = [
       # preview file tree when using cd widget
       # {} = directory being previewed
@@ -28,9 +32,10 @@
       # I usually use 'eza --tree' instead of tree but it doesn't seem to work in this case
     ];
 
-    defaultCommand = "${pkgs.bfs}/bin/bfs -nohidden -type f";
-    #defaultOptions = {};
-
+    # -- ctrl + t widget
+    # This is particularly useful with zsh suffixes
+    # because it allows you to simply search for the file with fzf and then hit enter twice to open it.
+    # Another usecase is 'git add ' <ctrl+t> search for file <enter>.
     fileWidgetCommand = "${pkgs.bfs}/bin/bfs -nohidden -type f";
     /*fileWidgetOptions = [
       "--preview 'head {}'"
@@ -40,6 +45,7 @@
       # and https://github.com/thimc/vifmimg for ideas on how to preview images
     ]; */
 
+    # ctrl + r widget
     #historyWidgetOptions = {}
   };
 }
