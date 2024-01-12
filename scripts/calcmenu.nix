64bba2d5969e9df751fbee26a7144adcd9db78b0
koalagang <74791721+koalagang@users.noreply.github.com>
2024-01-12 17:13:19 +0000

    Calcmenu's mechanism for calculating and add comments

--- scripts/calcmenu.nix
+++ scripts/calcmenu.nix
@@ -4,8 +4,19 @@
   home = {
     packages = [
       (pkgs.writeShellScriptBin "calcmenu" ''
+        # take user input using menu program of choice
+        # the colon here is just because some menus expect an input
+        # so we're simply giving it an empty input
         equation="$(: | ${config.home.sessionVariables.DMENU_CMD} -p 'Enter equation ')"
-        ${pkgs.libnotify}/bin/notify-send 'Result' -- "$(($equation))"
+
+        # use bc for the calculation and then round the number to two decimal places using printf
+        # finally, notify the user of the result
+        ${pkgs.libnotify}/bin/notify-send \
+          'Result' -- "$(${pkgs.coreutils}/bin/printf %.2f \
+          $(${pkgs.coreutils}/bin/echo "$equation" | ${pkgs.bc}/bin/bc -l))"
+
+        # it's important to note that, although bash itself can make calculations,
+        # it can't handle decimal places (hence why we're using bc)
       '')
     ];
   };
