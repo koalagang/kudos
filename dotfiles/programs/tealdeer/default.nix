5710ac490f94408b36152ceb02b966219dd83577
koalagang <74791721+koalagang@users.noreply.github.com>
2024-02-03 13:14:54 +0000

    Disable tealdeer cache auto-updating

--- dotfiles/programs/tealdeer/default.nix
+++ dotfiles/programs/tealdeer/default.nix
@@ -4,9 +4,12 @@
 
     settings = {
       display.compact = true;
-      update.auto_update = true;
+      update.auto_update = false;
       # TODO: nix-colors
       #style = {};
     };
+
+    # causes home-manager service to fail on boot when true
+    updateOnActivation = false;
   };
 }
