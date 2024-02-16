c565169eafcbe6ddea0bc08411bb60f20e90a59b
koalagang <74791721+koalagang@users.noreply.github.com>
2024-02-16 15:00:00 +0000

    Make pomo notify of time finished.

--- scripts/pomo.nix
+++ scripts/pomo.nix
@@ -12,11 +12,13 @@
         ${pkgs.timer}/bin/timer 45m
         ${pkgs.timewarrior}/bin/timew stop "$1" >/dev/null
         #${pkgs.speechd}/bin/spd-say 'Time for a break!' && ${pkgs.libnotify}/bin/notify-send 'Time for a break'
+        ${pkgs.coreutils}/bin/echo "Finished at $(${pkgs.coreutils}/bin/date +'%H:%M')"
       '')
 
       (pkgs.writeShellScriptBin "pomo-break" ''
         ${pkgs.timer}/bin/timer 15m
         #${pkgs.speechd}/bin/spd-say 'Break is over!' && ${pkgs.libnotify}/bin/notify-send 'Break is over!'
+        ${pkgs.coreutils}/bin/echo "Finished at $(${pkgs.coreutils}/bin/date +'%H:%M')"
       '')
     ];
   };
