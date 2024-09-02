787b55fe34ba4ca72735987b139abeb89c8bf7c0
koalagang <74791721+koalagang@users.noreply.github.com>
2024-09-02 13:34:22 +0100

    Remove some comments in zk config

--- dotfiles/programs/zk/default.nix
+++ dotfiles/programs/zk/default.nix
@@ -10,20 +10,24 @@
     settings = {
       note = {
         language = "en";
-        default-title = "untitled";
+        default-title = "Untitled";
         filename = "{{id}}-{{slug title}}";
         extension = "md";
-        # 36^4 different combinations available
         id-charset = "alphanum";
         id-length = 4;
         id-case = "lower";
       };
+
       tool = {
-        # TODO: figure out how to make a glow
         fzf-preview = "${pkgs.glow}/bin/glow --pager -s dark {-1}";
         # everything except for the bindings are default options
-        # (just have to add it because adding your own flags overrides the defaults)
+        # TODO: file an issue on the zk github about not being able to set custom fzf colours
         fzf-options = "--bind ctrl-u:preview-up,ctrl-d:preview-down --tiebreak begin --exact --tabstop 4 --height 100% --layout reverse --no-hscroll --color hl:-1,hl+:-1 --preview-window wrap";
+      };
+
+      alias = {
+        last = "zk edit --limit 1 --sort modified- $@";
+        recent = "zk edit --sort created- --created-after 'last week' --interactive";
       };
     };
   };
