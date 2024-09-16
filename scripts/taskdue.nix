e6e750f12e93cf8307c56ffee36008c9785c7d74
koalagang <74791721+koalagang@users.noreply.github.com>
2024-09-16 10:54:24 +0100

    Switch taskdue to using taskwarrior3 (forgot to do this when I changed the package used by the homemanager module)

--- scripts/taskdue.nix
+++ scripts/taskdue.nix
@@ -8,19 +8,19 @@
           day="$1"
           month="0$(($2+1))" # eww begins indexing the month at 0 for some reason
           year="$3"
-          DUE="$(${pkgs.taskwarrior}/bin/task due:$year-$month-$day list | ${pkgs.coreutils}/bin/tail -1)"
+          DUE="$(${pkgs.taskwarrior3}/bin/task due:$year-$month-$day list | ${pkgs.coreutils}/bin/tail -1)"
           [ -n "$DUE" ] && notify-send "$DUE due on $day/$month/$year"
         else
           # Notify of number of tasks overdue
-          OVERDUE="$(${pkgs.taskwarrior}/bin/task +OVERDUE list | ${pkgs.coreutils}/bin/tail -1)"
+          OVERDUE="$(${pkgs.taskwarrior3}/bin/task +OVERDUE list | ${pkgs.coreutils}/bin/tail -1)"
           [ -n "$OVERDUE" ] && ${pkgs.libnotify}/bin/notify-send 'taskwarrior' "You have $OVERDUE overdue"
 
           # Notify of number of tasks due today
-          TODAY="$(${pkgs.taskwarrior}/bin/task +TODAY list | ${pkgs.coreutils}/bin/tail -1)"
+          TODAY="$(${pkgs.taskwarrior3}/bin/task +TODAY list | ${pkgs.coreutils}/bin/tail -1)"
           [ -n "$TODAY" ] && ${pkgs.libnotify}/bin/notify-send 'taskwarrior' "You have $TODAY due TODAY"
 
           # Notify of number of tasks due at any point in time
-          ALL="$(${pkgs.taskwarrior}/bin/task due.any: list | ${pkgs.coreutils}/bin/tail -1)"
+          ALL="$(${pkgs.taskwarrior3}/bin/task due.any: list | ${pkgs.coreutils}/bin/tail -1)"
           [ -n "$ALL" ] && ${pkgs.libnotify}/bin/notify-send 'taskwarrior' "You have a total of $ALL due"
         fi
       '')
