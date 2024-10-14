f67a3e41b1032a2de083bc6928a8c899552f237d
koalagang <74791721+koalagang@users.noreply.github.com>
2024-10-14 13:11:31 +0100

    Extend timeout for hypridle

--- dotfiles/services/hypridle/default.nix
+++ dotfiles/services/hypridle/default.nix
@@ -4,18 +4,18 @@
     settings = {
       listener = [
         {
-          timeout = 90;
+          timeout = 120;
           on-timeout = "hyprlock";
         }
         {
-          timeout = 120;
+          timeout = 150;
           on-timeout = "hyprctl dispatch dpms off";
           on-resume = "hyprctl dispatch dpms on";
         }
         {
-          timeout = 150;
+          timeout = 180;
           # I've set the timer to be 1 minute via systemd-sleep (see the NixOS module).
-          # This means that my system will first suspend and then hibernate 1 minute after it suspends
+          # This means that my system will first suspend and then hibernate 1 minute after it suspends.
           on-timeout = "systemctl suspend-then-hibernate";
         }
       ];
