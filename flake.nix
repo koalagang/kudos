284790c259f8decf384ea8f7ae48eca54a8214a3
koalagang <74791721+koalagang@users.noreply.github.com>
2025-01-26 15:30:39 +0000

    Update flake inputs, including lanzaboote

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.1";
+      url = "github:nix-community/lanzaboote/v0.4.2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
