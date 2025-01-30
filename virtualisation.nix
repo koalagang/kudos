082bf55879256a83be785d04968ad131f5fdaee7
koalagang <74791721+koalagang@users.noreply.github.com>
2025-01-30 21:25:43 +0000

    Modify libvirt services

--- virtualisation.nix
+++ virtualisation.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   virtualisation.libvirtd = {
@@ -15,10 +15,14 @@
   };
   # GUI front-end
   programs.virt-manager.enable = true;
+
   # do not activate libvirtd service on startup (it will activate when launching virt-manager anyway)
-  systemd.services.libvirtd.wantedBy = pkgs.lib.mkForce [ ];
+  systemd.services.libvirtd.wantedBy = lib.mkForce [];
+  systemd.services.libvirt-guests.wantedBy = lib.mkForce [];
+
   # add user to the libvirtd group
   users.users.dante.extraGroups = [ "libvirtd" ];
+
   # persist VMs with nodatacow
   environment.persistence."/persist/nocow/system".directories = [ "/var/lib/libvirt" ];
 }
